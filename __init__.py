d984ad9a50d404597ba566dce7b4c5d255473da1
Li1177 <110624605+Li1177@users.noreply.github.com>
2025-08-02 17:13:38 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,13 @@
-from .custom_nodes import *
+from .custom_nodes import WanVideoEnhancer_F2
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
 WEB_DIRECTORY = "./web"
 
 NODE_CLASS_MAPPINGS = {
-    "WanVideoModelPatcher_F2"       : WanVideoModelPatcher_F2,
-    "WanVideoModelLoader_F2"        : WanVideoModelLoader_F2,
-    "WanVideoConfigure_F2"          : WanVideoConfigure_F2,
-    "WanVideoSampler_F2"            : WanVideoSampler_F2,
-    "WanVideoEnhancer_F2"           : WanVideoEnhancer_F2,
-    "ResizeImage_F2"                : ResizeImage_F2,
+    "CustomWanVideoEnhancer_F2": WanVideoEnhancer_F2
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "WanVideoModelPatcher_F2"       : "Wan Model Patcher",
-    "WanVideoModelLoader_F2"        : "Wan Model Loader",
-    "WanVideoConfigure_F2"          : "Wan Configure",
-    "WanVideoSampler_F2"            : "Wan Sampler",
-    "WanVideoEnhancer_F2"           : "Wan Frame Enhancer",
-    "ResizeImage_F2"                : "Resize Image",
+    "CustomWanVideoEnhancer_F2": "Custom Wan Video Enhancer (F2)"
 }
